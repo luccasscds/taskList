971eb39c6842ea322e12a47a4bea77c10ddc7a4a
luccasscds <lucassilvacds7@gmail.com>
2021-08-27 17:16:27 -0300

    mostra as quantidades de tarefas na coleçao

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -10,18 +10,18 @@ module.exports = {
         const collections = await Collections.get();
         const users = await User.get();
         const {userId, token} = req;
-        
         const userOnLine = users.find( user => {
             if(user.id === userId){
                 user.password = undefined
                 return user;
             }
         });
-
         const newCollections = collections.filter( collection => {
-            if(collection.userId == userId) return collection;
+            if(collection.userId == userId) {
+                collection.task = JSON.parse(collection.task);
+                return collection;
+            };
         });
-
         res.render('index', { user: userOnLine, token: token, collections: newCollections});
     }
 };
