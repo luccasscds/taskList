2ede45a65b44b147f0ea443145b85aea1b5cd473
luccasscds <lucassilvacds7@gmail.com>
2021-09-03 15:04:04 -0300

    adiciona o gitignore

--- public/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Sair da conta
-// function signOut() {
-//     const auth2 = gapi.auth2.getAuthInstance();
-
-//     auth2.signOut().then(function () {
-//         alert('Você desconectou da sua conta!')
-//     })
-// }
